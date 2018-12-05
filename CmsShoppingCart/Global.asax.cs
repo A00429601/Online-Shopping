4b3dabcbef32649efef23e357008874086ff17e8
kejifasuyi <43154234+kejifasuyi@users.noreply.github.com>
2018-12-05 04:02:57 +0000

    AuthenticateRequest done

--- CmsShoppingCart/Global.asax.cs
+++ CmsShoppingCart/Global.asax.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -18,7 +19,28 @@ namespace CmsShoppingCart
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             //mycode
                Database.SetInitializer<Db>(new DropCreateDatabaseIfModelChanges<Db>());
-
         }
+
+        protected void Application_AuthenticateRequest()
+        {
+            if (User == null) { return; }
+
+            string username = Context.User.Identity.Name;
+
+            string[] roles = null;
+
+            using (Db db = new Db())
+            {
+                UserDTO dto = db.Users.FirstOrDefault(x => x.Username == username);
+
+                roles = db.UserRoles.Where(x => x.UserId == dto.Id).Select(x => x.Role.Name).ToArray();
+
+                IIdentity userIdentity = new GenericIdentity(username);
+                IPrincipal newUserObj = new GenericPrincipal(userIdentity, roles);
+
+                Context.User = newUserObj;
+            }
+        }
+
     }
 }
