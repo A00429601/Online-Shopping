81d209c23d392fce640e969c3fc7525042a8e57f
A00429943_Maninder <maninderk770@gmail.com>
2018-12-04 21:38:54 -0400

    Updated Files
    for USers

---
+++ CmsShoppingCart/Views/Account/UserNavPartialVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CmsShoppingCart.Models.ViewModels.Account
+{
+    public class UserNavPartialVM
+    {
+        public string FirstName{ get; set; }
+        public string LasttName { get; set; }
+
+        public static implicit operator UserNavPartialVM(UserProfileVM v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
